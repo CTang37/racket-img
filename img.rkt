ddd95a1fec13b868b4df410c7d2365bc69ad381e
CTang 37 <claytontangct@gmail.com>
2026-06-15 22:28:07 -0400

    add examples

--- img.rkt
+++ img.rkt
@@ -7,12 +7,12 @@
 
 (define CHAR-OFFSET 300)
 
-(define x (rectangle 20 20 "solid" "orangered"))
-
 ; retrieves the color object corresponding to a color's name
 (define/contract (get-color color)
   (-> string? color?)
   (first (image->color-list (rectangle 1 1 "solid" color))))
+
+(check-equal? (get-color "red") (color 255 0 0 255))
 
 ; (colors->names Expr) : Expr
 (define-syntax colors->names
@@ -22,14 +22,6 @@
 
 (define colors (colors->names color-names))
 
-; converts a list of colors (representing an image) to text
-(define/contract (colors->text colors width width/acc)
-  (-> (listof color?) exact-nonnegative-integer? exact-nonnegative-integer? (listof char?))
-  (cond [(empty? colors) empty]
-        [(= width/acc (- width 0)) (cons #\newline (colors->text colors width 0))]
-        [else (cons (color->char (first colors))
-                                  (colors->text (rest colors) width (+ width/acc 1)))]))
-
 ; converts a color to a corresponding character
 (define/contract (color->char color)
   (-> color? char?)
@@ -37,12 +29,27 @@
     [(exact-nonnegative-integer? (index-of colors color)) (integer->char (+ CHAR-OFFSET (index-of colors color)))]
     [else (raise-syntax-error 'color "invalid image color" color)]))
 
+(check-equal? (color->char (color 0 255 0 255)) #\Ũ)
+
+; converts a list of colors (representing an image) to text
+; invariant: width/acc is the current width of the text row
+(define/contract (colors->text colors width width/acc)
+  (-> (listof color?) exact-nonnegative-integer? exact-nonnegative-integer? (listof char?))
+  (cond [(empty? colors) empty]
+        [(= width/acc (- width 0)) (cons #\newline (colors->text colors width 0))]
+        [else (cons (color->char (first colors))
+                                  (colors->text (rest colors) width (+ width/acc 1)))]))
+
+(check-equal? (colors->text (list (color 255 255 255 255) (color 0 0 0 255)) 2 0) '[#\Ư #\Ʒ])
+
 ; gets the dimensions of an image given text representation
 (define/contract (dims text)
   (-> string? (listof exact-nonnegative-integer?))
   (list (string-find text "\n") (length (string-split (string-replace text "\n" " ")))))
 
-; gets a color given the corresponding character
+(check-equal? (dims "ŨŨŨ\nŨŨŨ\n") '[3 2])
+
+; gets a color's name given the corresponding character
 (define/contract (char->color char)
   (-> char? string?)
   (cond
@@ -50,11 +57,7 @@
      (list-ref color-names (- (char->integer char) CHAR-OFFSET))]
     [else (raise-syntax-error 'color "character does not denote permitted color" char)]))
 
-; writes a list of characters
-(define (writec lst)
-  (cond
-    [(empty? lst) ""]
-    [else (write-char (first lst)) (writec (rest lst))]))
+(check-equal? (char->color #\ō) "yellow")
 
 ; (rep String) : Expr
 ; converts a textual representation to an image
